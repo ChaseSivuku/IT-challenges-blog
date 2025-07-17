b19a9df62489d46b68b125637f7e6b8ca38585ab
ChaseSivuku <113906831+ChaseSivuku@users.noreply.github.com>
2025-07-17 20:49:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showTip() {
+  const tip = document.getElementById("tech-tip");
+  tip.classList.toggle("hidden");
+}
+
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("active");
+}
